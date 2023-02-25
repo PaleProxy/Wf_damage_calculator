c21eeed72b28f5f0a6acaa87712f9388eef9d44f
PaleProxy <98685854+PaleProxy@users.noreply.github.com>
2023-02-25 22:24:24 +0200

    added basic faction info for logic

---
+++ Faction Damage/faction.py
@@ -0,0 +1,9 @@
+faction = (1 + 0.3)**2
+print(faction)
+
+'''
+    Modded Damage = 100 × (1 + 1.65) × (1 + 0.3) = 344.5
+    Tick damage = 0.35 × 344.5 × (1 + 0.3) = 156.7475
+
+As can be seen in the above calculations for tick damage, Faction Damage is applied twice, making their effective bonus = (1+Faction Bonus)^2. Which is +69% for the 30% Faction Damage mods and +140.25% for the 55% Primed Faction Damage mods. 
+'''
