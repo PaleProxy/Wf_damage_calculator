1a43cd43b6008fc9b990c0d61efa6d9b942ae8bf
PaleProxy <98685854+PaleProxy@users.noreply.github.com>
2023-02-28 19:57:45 +0200

    added basic quantum damage equation

---
+++ Quantum/quantum_calculations.py
@@ -0,0 +1,8 @@
+# https://warframe.fandom.com/wiki/Damage#Damage_Calculation
+mod_base_damage = 0
+
+quantum = mod_base_damage / 16
+
+total_damage_dealt = 0
+
+quantized_damage = round(total_damage_dealt / quantum), 0
